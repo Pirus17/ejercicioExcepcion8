14bc543670ebc065a2dfee889cca26032c6ecc82
Piruxb17 <Piruxb17@192.168.1.45>
2023-08-27 21:29:08 -0300

    terminado

---
+++ src/ejercicioexception8/EjercicioException8.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicioexception8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Piruxb17
+ */
+public class EjercicioException8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scr= new Scanner(System.in);
+        System.out.println("Ingrese una palabra: ");
+        String palabra= scr.nextLine();
+        System.out.println("Ingrese un numero: ");
+        int numero= scr.nextInt();
+        try{
+            System.out.println("la posisicion es: "+caracterEn(palabra,numero));
+           
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+        // TODO code application logic here
+    }
+    public static char caracterEn(String palabra, int numero ){
+        return palabra.charAt(numero) ;
+    }
+    
+}
